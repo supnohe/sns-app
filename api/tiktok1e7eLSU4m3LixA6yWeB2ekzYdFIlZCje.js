5f427f6f46485c74ac76438c1b9ba4746761d58e
supnohe <shion_kawasaki@yahoo.co.jp>
2026-08-21 15:01:44 +0900

    Create tiktok1e7eLSU4m3LixA6yWeB2ekzYdFIlZCje.js

---
+++ api/tiktok1e7eLSU4m3LixA6yWeB2ekzYdFIlZCje.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.status(200).send('tiktok-developers-site-verification=1e7eLSU4m3LixA6yWeB2ekzYdFIlZCje');
+}
